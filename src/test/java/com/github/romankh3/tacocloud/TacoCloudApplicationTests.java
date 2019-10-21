c81d6da01a285849ae97765d0855253fec38699d
romankh3 <roman.beskrovnyy@gmail.com>
2019-10-21 10:47:18 +0300

    Spring boot init.

---
+++ src/test/java/com/github/romankh3/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.romankh3.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
